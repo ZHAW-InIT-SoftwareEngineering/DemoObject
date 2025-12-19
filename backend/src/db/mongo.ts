581424befa376b671d5583ab7d1b9446691b6238
Cyril Gabriele <gabc@clt-mob-t-6292.zhaw.ch>
2025-12-19 13:51:52 +0100

    setup of the mongodb handling file i.e. return of the db and the collection as well as session establishment to the mongo server

--- backend/src/db/mongo.ts
+++ backend/src/db/mongo.ts
@@ -1,20 +1,38 @@
-import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
+import { MongoClient, Db, Collection } from "mongodb"; 
+import "dotenv/config";
+import { mkdtempDisposableSync } from "node:fs";
 
-export const collections: { DemoObjectCollection?: mongoDB.Collection } = {}
+let client: MongoClient | null = null; 
+let db: Db | null = null; 
 
-export async function connectToDatabase () {
-    dotenv.config();
+function validateEnvEntry(envVariableName: string): string {
+    const variable = process.env[envVariableName]
+    if(!variable) {
+        throw new Error(`${envVariableName} is not defined in the .env! Add a .env variable called: ${envVariableName}.`)
+    }
+        return variable
+};
 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-           
-    await client.connect();
-       
-    const db: mongoDB.Db = client.db(process.env.DB_NAME);
-  
-    const demoObjectCollection: mongoDB.Collection = db.collection(process.env.DEMO_OBJECT_COLLECTION_NAME);
+// currently db and connection uri is hard wired => however only one Db needed atm
+export async function connectToDb() {
+    if (db) return db; 
+    const uri = validateEnvEntry("DB_CONN_STRING")
+    const dbName = validateEnvEntry("DB_NAME")
+    client = new MongoClient(uri)
+    await client.connect()
+    db = client.db(dbName)
+    console.log(`SUCCESSFULLY CONNECTED TO ${dbName}`)
+    return db
+}
 
-    collections.DemoObjectCollection = demoObjectCollection;
-      
-    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${demoObjectCollection.collectionName}`);
+export function getDb(): Db {
+    if(!db) {
+        throw new Error("Database not initialized. Call connectToDatabase() first.")
+    } else{
+        return db
+    } 
+}
+
+export function getDbCollection<T = any>(dbCollectionName: string): Collection<T> {
+    return getDb().collection<T>(dbCollectionName);
 }
