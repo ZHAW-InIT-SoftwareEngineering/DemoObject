fe54e363701e069a425bc1be44a6e691e7147979
Cyril Gabriele <gabc@zhaw.ch>
2026-03-13 13:34:40 +0100

    deleted and ignored dist

--- backend/dist/persistence/mongo/session.db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SessionDb = void 0;
-const mongodb_1 = require("mongodb");
-const session_1 = require("../../domain/session");
-const zod_1 = require("zod");
-exports.SessionDb = session_1.Session.extend({
-    _id: zod_1.z.instanceof(mongodb_1.ObjectId),
-});
--- backend/dist/util/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidPath = void 0;
-var pathValidation_1 = require("./pathValidation");
-Object.defineProperty(exports, "isValidPath", { enumerable: true, get: function () { return pathValidation_1.isValidPath; } });
